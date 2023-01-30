778605479493015afcc7e13f430ef16f635001aa
o.sitnik <o.sitnik@cdek.ru>
2023-01-30 11:04:49 +0700

    feature LT-1: начал работу над автокомплитами и доработка журнала

--- src/main/java/my/goldgshok/life_tree/controller/AutocompleteController.java
+++ src/main/java/my/goldgshok/life_tree/controller/AutocompleteController.java
@@ -3,10 +3,12 @@ package my.goldgshok.life_tree.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("web/autocomplete")
 public class AutocompleteController {
 
     @PostMapping("get-persons-by-full-name")
@@ -14,7 +16,7 @@ public class AutocompleteController {
 
     }
 
-    @GetMapping
+    @GetMapping("get-genders")
     public void getGenders() {
 
     }
